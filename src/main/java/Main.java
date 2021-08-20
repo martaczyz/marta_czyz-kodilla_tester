4650f2d6f1610c7aa97690ec8119d86e0bb975a0
Marta Czyz <martaczyz9@gmail.com>
2021-08-20 13:21:59 +0200

    MainExtended

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String myName = "Marta";
+        int myAge = 36;
+        double height = 1.56;
+        char carClass = 'A';
+        boolean isGoodProgrammer = true;
+        System.out.println(myName);
+        String sayHello = "Hello from FirstClass";
+        System.out.println(sayHello);
+    }
+}
---
+++ src/main/java/MainExtended.java
@@ -0,0 +1,14 @@
+public class MainExtended {
+    public static void main(String [] args) {
+        String myAddress = "Lodz";
+        String street = "Mickiewicza";
+        int number = 37;
+        char entrance = '2';
+        boolean isNewLocator = true;
+        System.out.println(myAddress);
+        System.out.println(street);
+        System.out.println(number);
+        System.out.println(entrance);
+        System.out.println(isNewLocator);
+    }
+}
