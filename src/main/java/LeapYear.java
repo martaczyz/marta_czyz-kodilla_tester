7eec8221973a4dc8c39e902e9c77051da1f0e9c7
Marta Czyz <martaczyz9@gmail.com>
2021-08-25 11:51:12 +0200

    Next classes

--- src/main/java/LeapYear.java
+++ src/main/java/LeapYear.java
@@ -1,8 +1,9 @@
-public class LeapYear{
-
-        if ((2021 % 4 == 0 && 2021 % 100 = 0) || 2021 % 400 == 0) {
+public class LeapYear {
+    public static void main(String[] args) {
+        if ((2021 % 4 == 0 && 2021 % 100 == 0) || 2021 % 400 == 0) {
             System.out.println("Ten rok jest przestepny.");
         } else {
             System.out.println("Ten rok nie jest przestepny.");
         }
     }
+}
--- src/main/java/SimpleArray.java
+++ src/main/java/SimpleArray.java
@@ -1,5 +1,16 @@
- public class SimpleArray {
-     public static void main(String[] args) {
-         String[] movies = new String[]{"Emocje - krzyk do zrozumienia", "Radzenie sobie z trudnymi emocjami", "Czasoprzestrzenie", "Pięć języków miłości", "Garnek pełen strachu"};
-     }
- }
+public class SimpleArray {
+    public static void main(String[] args) {
+        String[] movies = new String[]{
+                "Emocje - krzyk do zrozumienia",
+                "Radzenie sobie z trudnymi emocjami",
+                "Czasoprzestrzenie",
+                "Pięć języków miłości",
+                "Garnek pełen strachu"
+        };
+        System.out.println(movies[2]);
+           // int numberOfElements = movies.length;
+            //    if numberOfElements == 5 {
+            //System.out.println("Moja tablica zawiera 5 elementów.");
+
+    }
+}
